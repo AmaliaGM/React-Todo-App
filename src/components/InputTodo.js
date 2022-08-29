c80316b62ef6be1cce85a3860a3bf6e81692f69e
amalia gomez moro <mariaamaliagomezmoro@gmail.com>
2022-08-29 17:56:34 -0300

    todo css

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -26,11 +26,16 @@ class InputTodo extends Component {
       
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Add todo..." 
-        value={this.state.title} onChange={this.onChange}
+        <form onSubmit={this.handleSubmit} className="form-container">
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add todo..."
+          value={this.state.title}
+          name="title"
+          onChange={this.onChange}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     )
   }
